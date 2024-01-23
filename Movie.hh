79740bfb3c33c5029d1aaf9523933c452f24c145
yurialvizi <yurialvizi@gmail.com>
2024-01-23 14:07:31 +0100

    add question 6

---
+++ Movie.hh
@@ -0,0 +1,50 @@
+#ifndef MOVIE_HH
+#define MOVIE_HH
+
+#include "Video.hh"
+
+class Movie : public Video
+{
+private:
+    int *chapters{};
+    int numberOfChapters{};
+
+public:
+    Movie() {};
+    Movie(string name, string fileName, int duration, int *chapters, int  numberOfChapters) : Video(name, fileName, duration), chapters(chapters), numberOfChapters(numberOfChapters){};
+
+    void setChapters(int *chapters, int numberOfChapters)
+    {
+        int *oldChapters = this->chapters;
+        delete[] oldChapters;
+        this->numberOfChapters = numberOfChapters;
+
+        this->chapters = new int[numberOfChapters];
+        copy(chapters, chapters + numberOfChapters, this->chapters);
+    }
+
+    int *getChapters() const
+    {
+        int *copyChapters = new int[numberOfChapters];
+        copy(chapters, chapters + numberOfChapters, copyChapters);
+        return copyChapters;
+    }
+
+    int getNumberOfChapters() const
+    {
+        return numberOfChapters;
+    }
+
+    void display(ostream &s) const override
+    {
+        Video::display(s);
+        s << "Chapters: " << endl;
+        for (int i = 0; i < getNumberOfChapters(); i++)
+        {
+            s << chapters[i] << endl;
+        }
+    }
+
+};
+
+#endif /* MOVIE_HH */
